9abe39f9bb50b9443d6de8680bd441421258f48c
Zuzeido <rodriguezinigor@gmail.com>
2021-10-18 15:23:08 +0200

    added Profesor model

---
+++ data/Profesor.php
@@ -0,0 +1,34 @@
+<?php
+
+class Post {
+    private $conn;
+    private $table = 'Profesor';
+
+    public $id;
+    public $category_id;
+
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+    
+    public function read () {
+        $query = 'SELECT
+        id,
+        nombre
+        FROM
+        ' . $this->table . '
+            ORDER BY
+            nombre ASC';
+
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+
+        return $stmt;
+    }
+}
+
+
+
+
+
+?>
